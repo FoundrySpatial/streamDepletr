516e69d0124ce9033a7033fb62e7d4e88dc2899f
Sam Zipper <samuelczipper@gmail.com>
2018-06-26 10:40:26 -0700

    Rename intermittent_pumping function

--- R/intermittent_pumping.R
+++ R/intermittent_pumping.R
@@ -1,4 +1,4 @@
-intermittentPumping <- function(t, starts, stops, rates, method="glover", d, S, Tr, ...){
+intermittent_pumping <- function(t, starts, stops, rates, method="glover", d, S, Tr, ...){
   #' Streamflow depletion for an intermittent pumping schedule using superposition.
   #' 
   #' @param t times you want output for [T]
@@ -14,7 +14,7 @@ intermittentPumping <- function(t, starts, stops, rates, method="glover", d, S, 
   #' (e.g. \link{glover}, \link{hunt}) this is not fractional depletion (\code{Qf}) because there can
   #' be different pumping rates at different times.
   #' @examples
-  #' intermittentPumping(t=seq(0,60,10), starts=0, stops=30, rates=100, method="glover", d=100, S=0.1, Tr=100)
+  #' intermittent_pumping(t=seq(0,60,10), starts=0, stops=30, rates=100, method="glover", d=100, S=0.1, Tr=100)
   #' @references
   #' Jenkins, C.T. (1968). Techniques for Computing Rate and Volume of Stream Depletion
   #' by Wells. Ground Water 6(2): 37-46. doi:10.1111/j.1745-6584.1968.tb01641.x
@@ -52,75 +52,3 @@ intermittentPumping <- function(t, starts, stops, rates, method="glover", d, S, 
   Q.out <- rowSums(Q.all)
   
 }
-
-# ## examples
-# require(ggplot2)
-# source("R/hunt.R")
-# source("R/glover.R")
-# source("R/HelperFunctions.R")
-# 
-# ## Rathfelder (2016) Figure 40
-# times <- seq(0,100)
-# starts <- 0
-# stops <- 30
-# rates <- 100
-# method <- "Glover"
-# S <- 0.25
-# Tr <- 10*20
-# 
-# df <- data.frame(t=times)
-# start.flag <- T
-# for (d in c(5, 50, 200)){
-#   df$Q <- intermittentPumping(t=times, starts=starts, stops=stops, rates=rates, method="glover", d=d, S=S, Tr=Tr)
-#   df$d <- d
-# 
-#   if (start.flag){
-#     df.all <- df
-#     start.flag <- F
-#   } else {
-#     df.all <- rbind(df.all, df)
-#   }
-# }
-# 
-# # plot
-# ggplot(df.all, aes(x=t, y=Q, color=factor(d))) +
-#   geom_line()
-# 
-# ## Rathfelder (2016) Figure 61
-# # inputs are in Rathfelder (2016) Table 8
-# times <- seq(0,730)
-# starts <- c(0,366)
-# stops <- c(65, 430)
-# rates <- rep(100, length(starts))
-# d <- 167
-# S <- 0.09
-# Tr <- 640
-# 
-# # streambed properties needed to calculate lambda for Hunt
-# str_width <- 60
-# str_cond <- 3
-# str_thick <- 1
-# lmda <- str_width*str_cond/str_thick
-# 
-# df <- data.frame(t=times)
-# start.flag <- T
-# for (method in c("glover", "hunt")){
-#   if (method=="glover"){
-#     df$Q <- intermittentPumping(t=times, starts=starts, stops=stops, rates=rates, method=method, d=d, S=S, Tr=Tr)
-#   } else if (method=="hunt"){
-#     df$Q <- intermittentPumping(t=times, starts=starts, stops=stops, rates=rates, method=method, d=d, S=S, Tr=Tr, lmda=lmda)
-#   }
-# 
-#   df$method <- method
-# 
-#   if (start.flag){
-#     df.all <- df
-#     start.flag <- F
-#   } else {
-#     df.all <- rbind(df.all, df)
-#   }
-# }
-# 
-# ggplot(df.all, aes(x=t, y=Q, color=method)) +
-#   geom_line() +
-#   scale_y_continuous(limits=c(0,100))
