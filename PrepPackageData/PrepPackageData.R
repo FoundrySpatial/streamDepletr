1187fdc4e27493ce1dff3dbedad34f0a8f43f000
Sam Zipper <samuelczipper@gmail.com>
2018-07-13 10:42:10 -0700

    Add some example data from Wisconsin which will be used in vignette

---
+++ PrepPackageData/PrepPackageData.R
@@ -0,0 +1,122 @@
+## PrepPackageData.R
+#' This script is intended to prepare the data that will be contained within the package:
+#'  -Stream network shapefile (Sixmile Creek upstream of Lake Mendota, Wisconsin)
+#'  -Daily streamflow from two USGS NWIS stations:
+#'    (i) DORN (SPRING) CREEK AT CT HIGHWAY M NR WAUNAKEE,WI (05427930), data available 2012-07-04 - present
+#'    (ii) SIXMILE CREEK @ COUNTY TRNK HGHWY M NR WAUNAKEE,WI (05427910), data avaiable 2012-07-26 - present
+
+## required packages
+require(rgdal)
+require(raster)
+require(dplyr)
+require(magrittr)
+require(waterData)
+require(ggplot2)
+require(devtools)
+require(broom)
+require(sp)
+require(gridExtra)
+
+## Prep stream network shapefile
+# load data, which was downloaded from the National Map Viewer (https://viewer.nationalmap.gov/advanced-viewer/)
+# on 2018-07-13 for HUC07090002, trimmed to only Sixmile Creek, and reprojected to EPSG:26916
+stream_shp <- 
+  readOGR(dsn = "PrepPackageData", 
+          layer = "NHDPlusV21_Flowline_SixmileCreek",
+          stringsAsFactors=F)
+
+# take a look
+plot(stream_shp)
+
+# only keep useful columns
+stream_shp@data <-
+  stream_shp@data %>% 
+  subset(select=REACHCODE) %>% 
+  set_colnames("reach")
+
+# which reaches are part of Dorn Creek?
+# this was determine by manually inspecting the stream network in QGIS
+reaches_dorn <- 
+  c("070900002008377",
+    "070900002007664",
+    "070900002007665",
+    "070900002007666",
+    "070900002007667",
+    "070900002007668",
+    "070900002007669",
+    "070900002007670",
+    "070900002007671",
+    "070900002007672")
+
+# make a column with stream name
+stream_shp@data$stream <- "Sixmile Creek"
+stream_shp@data$stream[stream_shp@data$reach %in% reaches_dorn] <- "Dorn Creek"
+
+## download streamflow data
+df_dorn <- 
+  importDVs("05427930", code="00060", stat="00003", sdate="2013-10-01", edate="2015-09-30") %>% 
+  dplyr::select(dates, val) %>% 
+  transform(Q_m3d = val*0.3048*0.3048*0.3048*86400,  # convert discharge to cubic meters/day
+            stream = "Dorn Creek") %>% 
+  dplyr::select(dates, Q_m3d, stream) %>% 
+  set_colnames(c("date", "Q_m3d", "stream"))
+
+df_sixmile <- 
+  importDVs("05427910", code="00060", stat="00003", sdate="2013-10-01", edate="2015-09-30") %>% 
+  dplyr::select(dates, val) %>% 
+  transform(Q_m3d = val*0.3048*0.3048*0.3048*86400,  # convert discharge to cubic meters/day
+            stream = "Sixmile Creek") %>% 
+  dplyr::select(dates, Q_m3d, stream) %>% 
+  set_colnames(c("date", "Q_m3d", "stream"))
+
+# merge into one data frame
+discharge_df <- rbind(df_sixmile, df_dorn)
+
+# get lat/lon of gauging stations and reproject to EPSG:26916
+df_info <- siteInfo(c("05427930", "05427910"))
+xy <- df_info[,c("lng", "lat")]
+spdf_info <- 
+  SpatialPointsDataFrame(coords = xy, data = df_info,
+                         proj4string = CRS("+init=epsg:4326")) %>% 
+  spTransform(CRS("+init=epsg:26916")) %>% 
+  subset(select=c("staid", "staname")) %>% 
+  as.data.frame()
+
+## save data for use in package
+devtools::use_data(stream_shp, discharge_df, overwrite=T)
+
+## make plots
+# map
+stream_df <- tidy(stream_shp)
+p.map <- 
+  ggplot() +
+  geom_path(data=stream_df, aes(x=long, y=lat, group=group), color="blue") +
+  geom_point(data=spdf_info, aes(x=lng, y=lat)) +
+  annotate("text", x=spdf_info$lng[1], y=spdf_info$lat[1]-400, label=spdf_info$staid[1]) +
+  annotate("text", x=spdf_info$lng[2]+1400, y=spdf_info$lat[2], label=spdf_info$staid[2]) +
+  scale_x_continuous(name="UTM 16N Easting [m]") +
+  scale_y_continuous(name="UTM 16N Northing [m]") +
+  labs(title="Sixmile Creek Watershed", subtitle="North of Lake Mendota, WI") +
+  #coord_equal() +
+  theme_bw() +
+  theme(panel.grid=element_blank(),
+        axis.text.y=element_text(angle = 90, hjust = 0.5))
+
+# discharge
+p.Q <- 
+  ggplot(discharge_df, aes(x=date, y=Q_m3d)) +
+  geom_line(color="blue") +
+  scale_y_log10(name="Discharge [cubic meters/day]") +
+  scale_x_date(name="Date", expand=c(0,0)) +
+  facet_wrap(~stream, ncol=1, 
+             labeller=as_labeller(c("Dorn Creek"=paste0(spdf_info$staid[1], ", Dorn Creek"),
+                                    "Sixmile Creek"=paste0(spdf_info$staid[2], ", Sixmile Creek")))) +
+  theme_bw() +
+  theme(panel.grid=element_blank(),
+        axis.text.y=element_text(angle = 90, hjust = 0.5),
+        strip.background=element_blank())
+
+# save map for use in vignette
+ggsave(file.path("vignettes", "Map+Discharge.png"),
+       grid.arrange(p.map, p.Q, ncol=2, widths=c(1.5,1)),
+       width=8, height=6, units="in")
---
+++ data/data.R
@@ -0,0 +1,27 @@
+#' Stream network for Sixmile Creek Watershed, WI.
+#'
+#' @format A SpatialLinesDataFrame with 49 rows and 2 variables:
+#' \describe{
+#'   \item{reach}{identifier code for each stream reach}
+#'   \item{stream}{name of stream for each stream reach (Sixmile Creek or Dorn Creek)}
+#'   ...
+#' }
+#' @source \url{http://www.horizon-systems.com/NHDPlusData/NHDPlusV21/Data/NationalData/NHDPlusV21_NationalData_CONUS_Seamless_Geodatabase_05.7z}
+"stream_shp"
+
+#' Streamflow for Sixmile Creek and Dorn Creek.
+#' 
+#' Daily discharge data for the 2014-2015 water years for the US Geological Survey
+#' gauging stations DORN (SPRING) CREEK AT CT HIGHWAY M NR WAUNAKEE,WI (station ID 05427930)
+#' and SIXMILE CREEK @ COUNTY TRNK HGHWY M NR WAUNAKEE,WI (station ID 05427910).
+#'
+#' @format A data frame with 1450 rows and 2 variables:
+#' \describe{
+#'   \item{date}{date of streamflow measurement}
+#'   \item{Q_m3d}{discharge, in cubic meters per day}
+#'   \item{stream}{name of stream for each stream reach (Sixmile Creek or Dorn Creek)}
+#'   ...
+#' }
+#' @source \url{https://waterdata.usgs.gov/nwis/dv?referred_module=sw&agency_code=USGS&site_no=05427910}
+#' @source \url{https://waterdata.usgs.gov/nwis/dv?referred_module=sw&agency_code=USGS&site_no=05427930}
+"discharge_df"
